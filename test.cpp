7aaf7cf3b3505b4f16d65f216b74cc14c8753786
matejaRaskovic <mateja.raskovic@insidemaps.com>
2021-03-17 23:23:28 +0100

    implemented compression and decompression functions and added tests for them

--- test.cpp
+++ test.cpp
@@ -9,7 +9,7 @@ int main(){
     int num_passed_1 = 0, total_1 = 0;
 
     // test 1
-    std::cout << "Test 1:" << std::endl;
+    std::cout << "Test 1:" << std::endl << std::endl;
     passed = true;
     uint64_t t1_1 = 13579;
     std::cout << "Input: " << t1_1 << std::endl;
@@ -37,7 +37,7 @@ int main(){
     total_1++;
 
     // test 2
-    std::cout << "Test 2:" << std::endl;
+    std::cout << "Test 2:" << std::endl << std::endl;
     passed = true;
     uint64_t t1_2 = 0;
     std::cout << "Input: " << t1_2 << std::endl;
@@ -65,7 +65,7 @@ int main(){
     total_1++;
 
     // test 3
-    std::cout << "Test 3:" << std::endl;
+    std::cout << "Test 3:" << std::endl << std::endl;
     passed = true;
     uint64_t t1_3 = ~0;
     std::cout << "Input: " << t1_3 << std::endl;
@@ -99,7 +99,7 @@ int main(){
     int num_passed_2 = 0, total_2 = 0;
 
     // test 1
-    std::cout << "Test 1:" << std::endl;
+    std::cout << "Test 1:" << std::endl << std::endl;
     passed = true;
     std::vector<uint8_t> t2_1{0xEA, 0x0B};
     std::cout << "Input: ";
@@ -121,7 +121,7 @@ int main(){
     total_2++;
 
     // test 2
-    std::cout << "Test 2:" << std::endl;
+    std::cout << "Test 2:" << std::endl << std::endl;
     passed = true;
     std::vector<uint8_t> t2_2{0x01};
     std::cout << "Input: ";
@@ -143,7 +143,7 @@ int main(){
     total_2++;
 
     // test 3
-    std::cout << "Test 2:" << std::endl;
+    std::cout << "Test 3:" << std::endl << std::endl;
     passed = true;
     std::vector<uint8_t> t2_3{0x81, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F};
     std::cout << "Input: ";
@@ -165,6 +165,49 @@ int main(){
     total_2++;
 
     std::cout << "Passed " << num_passed_2 << "/" << total_2 << std::endl << std::endl;
+
+
+    std::cout << "<==========     TESTING SEQUENCE COMPRESSION + DECOMPRESSION     ==========>" << std::endl << std::endl;
+
+    int num_tests = 10;
+    int total_passed = 0;
+    bool passed_comp_decomp[10];
+    srand(37);
+
+    uint64_t seq_starts[num_tests] = {24234, 3453823, 11111, 8, 8433, 97397, 987654321, 157, 736492, 20202021};
+    for (int i = 0; i < num_tests; i++){
+        std::cout << "Test " << i << ": " << std::endl << std::endl;
+        passed_comp_decomp[i] = true;
+        int seq_len = rand() % 10 + 5;
+        std::vector<uint64_t> original;
+        original.push_back(seq_starts[i]);
+        for (int j = 1; j < seq_len; j++){
+            original.push_back(original[j-1] + (rand() % 10000 + 5));
+        }
+        std::vector<uint8_t> compressed = compress(original);
+        std::vector<uint64_t> decompressed = decompress(compressed);
+        std::cout << "Input:  ";
+        for (size_t k = 0; k < original.size(); k++){
+            std::cout << int(original[k]) << " ";
+        }
+        std::cout << std::endl << "Output: ";
+        for (size_t k = 0; k < decompressed.size(); k++){
+            std::cout << int(decompressed[k]) << " ";
+        }
+
+        if (decompressed.size() == original.size()){
+            for (size_t k = 0; k < original.size(); k++){
+                if (decompressed[k] != original[k]) passed_comp_decomp[i] = false;
+            }
+        } else {
+            passed_comp_decomp[i] = false;
+        }
+        std::cout << std::endl << std::endl;
+        std::cout << (passed_comp_decomp[i] ? "PASSED" : "FAILED") << std::endl << std::endl << std::endl;
+        total_passed += passed_comp_decomp[i];
+    }
+
+    std::cout << "Passed " << total_passed << "/" << num_tests << std::endl << std::endl;
 
     return 0;
 }
--- varint_compression.cpp
+++ varint_compression.cpp
@@ -30,7 +30,22 @@ std::vector<uint8_t> compress_single(uint64_t original){
 
 
 std::vector<uint8_t> compress(std::vector<uint64_t> original){
+    std::vector<uint8_t> varint_compressed;
+    uint64_t prev = 0;
+    for (size_t i = 0; i < original.size(); i++){
+        if (i) {
+            prev = original[i-1];
+            if (prev >= original[i]){
+                // original vector is not monotonically increasing an exception should be raised or similar
+            }
+        }
+        std::vector<uint8_t> single_varint_compressed = compress_single(original[i] - prev);
+        for (size_t j = 0; j < single_varint_compressed.size(); j++){
+            varint_compressed.push_back(single_varint_compressed[j]);
+        }
+    }
 
+    return varint_compressed;
 }
 
 
@@ -47,5 +62,22 @@ uint64_t decompress_single(std::vector<uint8_t> compressed){
 
 
 std::vector<uint64_t> decompress(std::vector<uint8_t> compressed){
+    std::vector<uint64_t> decompressed;
+    std::vector<uint8_t> single_to_decompress;
+    for (size_t i = 0; i < compressed.size(); i++){
+        uint8_t current = compressed[i];
+        single_to_decompress.push_back(current);
+        if (current & (1 << 7)){
+            continue;
+        } else {
+            uint64_t to_add = decompress_single(single_to_decompress);
+            decompressed.push_back(to_add);
+            if (decompressed.size() > 1){
+                decompressed[decompressed.size()-1] += decompressed[decompressed.size()-2];
+            }
+            single_to_decompress.clear();
+        }
+    }
 
+    return decompressed;
 }
